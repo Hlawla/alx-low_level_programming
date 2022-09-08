93d61ff42daa86a2f532af4a8c280c5b9cb98395
Hlawla <Hsshilaluke@gmail.com>
2022-09-08 13:57:15 -0700

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/**  
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "programming is like building multilingual puzzel"
-* Return: 0
-*/
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	printf("with proper grammar, but the outcome is a piece of
+
+			art,\n");
+
+	return (0);
+}
