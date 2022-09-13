200be18d79f73015ce332722bbc84d08722209d8
Hlawla <Hsshilaluke@gmail.com>
2022-09-12 20:45:17 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * main - lowercase and uppercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a';c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
