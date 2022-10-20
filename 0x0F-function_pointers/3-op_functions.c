62c9c0bc724364994edf845f94a87a718c59486e
Hlawla <Hsshilaluke@gmail.com>
2022-10-20 12:55:41 -0700

    Task 2

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+e45r44r4e5r4z [5D 0OF
+
